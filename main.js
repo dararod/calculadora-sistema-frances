54ccf8cf6f7443f1e27087b8c5d98aa0dad53768
dararod <dalejorod@gmail.com>
2023-11-29 22:47:47 -0400

    tercera entrega 2

--- main.js
+++ main.js
@@ -1,116 +1,115 @@
 // Toma los inputs del HTML 
-let inputValorInicialPrestamo = document.getElementById("prestamo");
-let inputTasaPactada = document.getElementById("tasa");
-let inputCantidadCuotas = document.getElementById("cuotas");
+const inputValorInicialPrestamo = document.getElementById("prestamo");
+const inputTasaPactada = document.getElementById("tasa");
+const inputCantidadCuotas = document.getElementById("cuotas");
 
+const LOCAL_STORAGE_KEY = "Valores";
+const BOTON_ENVIAR_FORM = document.getElementById("button");
+const BOTON_LIMPIAR_CACHE = document.getElementById("limpiarCache");
+const TABLE_ELEMENT = document.getElementById("listaCuotas");
 
-let contador = 0
-let boton = document.getElementById("button");
-
-boton.onclick = () => {
-    contador++
+function calcularCuota(prestamo, interes, cuotas) {
+    return prestamo * interes / (1 - Math.pow((1 + interes), -cuotas));
 }
 
-boton.addEventListener('click', (event) => {
-    event.preventDefault();
+function crearCuota(numeroCuota, interes, amortizacion, saldo, cuota) {
+    return {
+        numeroCuota,
+        interes,
+        amortizacion,
+        saldo,
+        cuota
+    }
+}
 
-// Extrae valores de los inputs
+function generarListaCuotas(valorInicialPrestamo, cantidadCuotas, tasaPactada) {
+    const tasa = tasaPactada / 100;
+    const listaCuotas = [];
+    const cuota = calcularCuota(valorInicialPrestamo, tasa, cantidadCuotas);
+
+    let valorPrestamo = valorInicialPrestamo;
+
+    for (let i = 0; i < cantidadCuotas; i++) {
+        let valorInteres = valorPrestamo * (1 + tasa) - valorPrestamo;
+        let valorAmortizacion = cuota - valorInteres;
+
+        valorPrestamo -= valorAmortizacion;
+
+        const tablaCuotas = crearCuota(i + 1, valorInteres, valorAmortizacion, valorPrestamo, cuota)
+
+        listaCuotas.push(tablaCuotas);
+    }
+
+    return listaCuotas;
+}
+
+function guardarEnCache(listaCuotas) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(listaCuotas));
+}
+
+function cargarDeCache() {
+    const data = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    if (data) {
+        return JSON.parse(data);
+    }
+
+    return [];
+}
+
+function mostrarCuotas(listaCuotas) {
+    listaCuotas.forEach((value) => {
+        const tr = document.createElement("tr");
+        const tdNCuota = document.createElement("td");
+        const tdCapital = document.createElement("td");
+        const tdAmortizacion = document.createElement("td");
+        const tdIntereses = document.createElement("td");
+        const tdCuota = document.createElement("td");
+
+        tr.appendChild(tdNCuota);
+        tr.appendChild(tdCapital);
+        tr.appendChild(tdAmortizacion);
+        tr.appendChild(tdIntereses);
+        tr.appendChild(tdCuota);
+
+        tdNCuota.textContent = value.numeroCuota;
+        tdCapital.textContent = value.saldo.toFixed(2);
+        tdAmortizacion.textContent = value.amortizacion.toFixed(2);
+        tdIntereses.textContent = value.interes.toFixed(2);
+        tdCuota.textContent = value.cuota.toFixed(2);
+
+        TABLE_ELEMENT.appendChild(tr);
+    });
+}
+
+function limpiarTabla() {
+    while (TABLE_ELEMENT.firstChild) {
+        TABLE_ELEMENT.removeChild(TABLE_ELEMENT.firstChild);
+    }
+}
+
+function calcularCuotasYMostrar() {
     const valorInicialPrestamo = inputValorInicialPrestamo.valueAsNumber;
     const tasaPactada = inputTasaPactada.valueAsNumber;
     const cantidadCuotas = inputCantidadCuotas.valueAsNumber;
+    const cuotas = generarListaCuotas(valorInicialPrestamo, cantidadCuotas, tasaPactada);
 
-    function calcularCuota(prestamo, interes, cuotas) {
-        return prestamo * interes / (1 - Math.pow((1 + interes), -cuotas));
-    }
+    limpiarTabla();
+    mostrarCuotas(cuotas);
+    guardarEnCache(cuotas);
+}
 
-    function crearTablaCuotas(numeroCuota, interes, amortizacion, saldo, cuota) {
-        return {
-            numeroCuota,
-            interes,
-            amortizacion,
-            saldo,
-            cuota
-        }
-    }
+BOTON_ENVIAR_FORM.addEventListener('click', (event) => {
+    event.preventDefault();
+    calcularCuotasYMostrar();
+});
 
-    function listarCuotas(cantidadCuotas, tasaPactada) {
-        const tasa = tasaPactada / 100;
-        const listaCuotas = [];
+BOTON_LIMPIAR_CACHE.addEventListener('click', () => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+});
 
-        let valorPrestamo = valorInicialPrestamo;
-        let cuota = calcularCuota(valorInicialPrestamo, tasa, cantidadCuotas);
-        cuota = cuota.toFixed(2);
-        for (let i = 0; i < cantidadCuotas; i++) {
-            let valorInteres = valorPrestamo * (1 + tasa) - valorPrestamo;
-            let valorAmortizacion = cuota - valorInteres;
-            valorPrestamo -= valorAmortizacion;
+window.addEventListener('load', () => {
+    const cuotas = cargarDeCache();
 
-            valorInteres = valorInteres.toFixed(2);
-            valorAmortizacion = valorAmortizacion.toFixed(2);
-            valorPrestamo.toFixed(2);
-            const tablaCuotas = crearTablaCuotas(i + 1, valorInteres, valorAmortizacion, valorPrestamo, cuota)
-
-            listaCuotas.push(tablaCuotas);
-        }
-
-        return listaCuotas;
-    }
-
-    const cuotas = listarCuotas(cantidadCuotas, tasaPactada);
-
-    const tableBody = document.getElementById("tbody");
-
-    localStorage.setItem("Valores", JSON.stringify(cuotas))
-    let lSTable = JSON.parse(localStorage.getItem("Valores"))
-
-    if (lSTable != [{}] && contador === 0){
-        lSTable.forEach(value => {
-            const tr = document.createElement("tr");
-        
-                const tdNCuota = document.createElement("td");
-                const tdCapital = document.createElement("td");
-                const tdAmortizacion = document.createElement("td");
-                const tdIntereses = document.createElement("td");
-                const tdCuota = document.createElement("td");
-        
-                tr.appendChild(tdNCuota);
-                tr.appendChild(tdCapital);
-                tr.appendChild(tdAmortizacion);
-                tr.appendChild(tdIntereses);
-                tr.appendChild(tdCuota);
-        
-                tdNCuota.textContent = value.numeroCuota;
-                tdCapital.textContent = value.saldo;
-                tdAmortizacion.textContent = value.amortizacion;
-                tdIntereses.textContent = value.interes;
-                tdCuota.textContent = value.cuota;
-                tableBody.appendChild(tr);
-        })
-    }
-
-//  Crea por cada cuota un <tr> y lo agrega al HTML
-    cuotas.forEach(value => {
-        if (contador === 1) {
-            const tr = document.createElement("tr");
-    
-            const tdNCuota = document.createElement("td");
-            const tdCapital = document.createElement("td");
-            const tdAmortizacion = document.createElement("td");
-            const tdIntereses = document.createElement("td");
-            const tdCuota = document.createElement("td");
-    
-            tr.appendChild(tdNCuota);
-            tr.appendChild(tdCapital);
-            tr.appendChild(tdAmortizacion);
-            tr.appendChild(tdIntereses);
-            tr.appendChild(tdCuota);
-    
-            tdNCuota.textContent = value.numeroCuota;
-            tdCapital.textContent = value.saldo;
-            tdAmortizacion.textContent = value.amortizacion;
-            tdIntereses.textContent = value.interes;
-            tdCuota.textContent = value.cuota;
-            tableBody.appendChild(tr);
-        }
-    })
+    mostrarCuotas(cuotas);
 });
